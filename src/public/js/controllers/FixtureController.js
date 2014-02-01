57015e09d1a34df702ded1ad3e22803c5ecd995b
Ashwin Ramesh <ashramesh1992@gmail.com>
2014-02-02 10:37:54 +1100

    index page done

--- src/public/js/controllers/FixtureController.js
+++ src/public/js/controllers/FixtureController.js
@@ -1,4 +1,15 @@
-app.controller("FixtureController", function($scope, $location, getFixtures) {
-    $scope.fixtureData = getFixtures.fixtures();
-    console.log($scope.fixtureData);
-})
+app.controller('FixtureController', function($scope, getFixtures) {
+    $scope.fixtureData = getFixtures.fixtures().then(function(fixtureData) {
+        for (var fixture in fixtureData.fixtures) {
+            var obj = fixtureData.fixtures[fixture];
+            console.log(obj);
+            if (obj.home_goals == null || obj.away_goals == null) {
+                fixtureData.fixtures[fixture].result = "T.B.D.";
+            } else {
+                fixtureData.fixtures[fixture].result = fixtureData.fixtures[fixture].home_goals + "-" + fixtureData.fixtures[fixture].away_goals;
+            }
+        }
+        $scope.fixtureData = fixtureData;
+        console.log($scope.fixtureData);
+    });
+});
